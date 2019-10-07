32f10537d8019bacef20a342e36566460ad643b4
jparedes <manuelsederap@gmail.com>
2019-10-07 23:25:03 +0800

    adding-the-order-button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,8 +17,20 @@ class BurgerBuilder extends Component {
 			cheese: 0,
 			meat: 0
 		},
-		totalPrice: 4
+		totalPrice: 4,
+		purchasable: false
 	}
+
+	updatePurchasable (ingredients) {
+		const sum = Object.keys(ingredients).map(igKey => {
+			return ingredients[igKey]
+		}).reduce((sum, el) => {
+			return sum + el;
+		}, 0);
+
+		this.setState({purchasable: sum > 0})
+	}
+
 
 	addIngredientHandler = (type) => {
 		const oldCount = this.state.ingredients[type];
@@ -34,6 +46,7 @@ class BurgerBuilder extends Component {
 			totalPrice: newPrice,
 			ingredients: updatedIngredients
 		})
+		this.updatePurchasable(updatedIngredients);
 	}
 
 	removeIngredientHandler = (type) => {
@@ -53,6 +66,7 @@ class BurgerBuilder extends Component {
 			totalPrice: newPrice,
 			ingredients: updatedIngredients
 		})
+		this.updatePurchasable(updatedIngredients);
 	}
 
 	render() {
@@ -71,6 +85,7 @@ class BurgerBuilder extends Component {
 					ingredientAdded={this.addIngredientHandler}
 					ingredientRemoved={this.removeIngredientHandler}
 					disabled={disabledInfo}
+					purchasable={this.state.purchasable}
 					price={this.state.totalPrice}
 				/>
 			</Auxillary>
